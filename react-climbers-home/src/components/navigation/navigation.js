2264878a15fb1f598794c27f34d3f8d0164371ea
Philip Lutz <philiplutz413@gmail.com>
2019-01-31 19:09:22 -0800

    started logo insertion

--- react-climbers-home/src/components/navigation/navigation.js
+++ react-climbers-home/src/components/navigation/navigation.js
@@ -1,10 +1,22 @@
-import React 	from 'react';
+import React 						from 'react';
+import { Link } 					from 'react-router-dom';
+import Logo							from '../logo.js';
 import './navigation.css';
 
 export default class Navigation extends React.Component {
 	render() {
+
 		return (
-			<div>Navigation</div>
+			<nav id="navigation">
+				<Logo />
+				<ul>
+					<li>1</li>
+					<li>2</li>
+					<li>3</li>
+					<li>4</li>
+					<li>5</li>
+				</ul>
+			</nav>
 		);
 	}
 }
