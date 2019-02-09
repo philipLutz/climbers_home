b42ef4f7f854e2adad29f2df9624f377269bcf06
Philip Lutz <philiplutz413@gmail.com>
2019-02-09 17:41:34 -0600

    fixed hover on nav logo

--- react-climbers-home/src/components/navigation/navigation.js
+++ react-climbers-home/src/components/navigation/navigation.js
@@ -1,16 +1,36 @@
 import React 						from 'react';
 import { Link } 					from 'react-router-dom';
-import Logo							from '../logo.js';
 import Burger						from './burger.js';
+import Logo							from '../logo.js';
 import './navigation.css';
 
 export default class Navigation extends React.Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			fill: "#fff"
+		}
+	}
+
+	mouseEnter = () => {
+		this.setState({
+			fill: "#61616B"
+		});
+	}
+
+	mouseLeave = () => {
+		this.setState({
+			fill: "#fff"
+		});
+	}
+	
 	render() {
 		return (
 			<nav id="navigation">
-				<div id="logo-container">
+				<div id="logo-container" onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave}>
 					<Link to="/">
-						<Logo id="logo" width="100%" height="100%" fill="#fff" viewBox="0 0 800 800" />
+						<Logo id="logo" width="100%" height="100%" fill={this.state.fill} viewBox="0 0 800 800" />
 					</Link>
 				</div>
 				<h3>Climber's Home</h3>
