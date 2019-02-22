bb5477b9a5b75aa023e95799e118412827d589d5
Philip Lutz <philiplutz413@gmail.com>
2019-02-22 14:32:05 -0800

    added onclick close of nav when link is used

--- react-climbers-home/src/components/navigation/burger.js
+++ react-climbers-home/src/components/navigation/burger.js
@@ -7,25 +7,48 @@ export default class Burger extends React.Component {
     event.preventDefault();
   }
 
+  constructor (props) {
+    super(props);
+    this.state = {
+      menuOpen: false
+    }
+  }
+
+  handleStateChange (state) {
+      this.setState({menuOpen: state.isOpen})  
+  }
+
+  closeMenu () {
+      this.setState({menuOpen: false})
+  }
+
+  toggleMenu () {
+    this.setState({menuOpen: !this.state.menuOpen})
+  }
+
   render () {
     return (
-      <Menu right width={ '28%' }>
-        <Link to="/">
+      <Menu 
+        right width={ '28%' }
+        isOpen={this.state.menuOpen}
+        onStateChange={(state) => this.handleStateChange(state)}
+      >
+        <Link to="/" onClick={() => this.closeMenu()}>
           <h5>Home</h5>
         </Link>
-        <Link to="/amenities">
+        <Link to="/amenities" onClick={() => this.closeMenu()}>
           <h5>Amenities</h5>
         </Link>
-        <Link to="/rooms">
+        <Link to="/rooms" onClick={() => this.closeMenu()}>
           <h5>Rooms</h5>
         </Link>
-        <Link to="/calendar">
+        <Link to="/calendar" onClick={() => this.closeMenu()}>
           <h5>Calendar</h5>
         </Link>
-        <Link to="/beta">
+        <Link to="/beta" onClick={() => this.closeMenu()}>
           <h5>Beta</h5>
         </Link>
-        <Link to="/contact">
+        <Link to="/contact" onClick={() => this.closeMenu()}>
           <h5>Contact</h5>
         </Link>
       </Menu>
