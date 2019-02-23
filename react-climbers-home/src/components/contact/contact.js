013e0c5498284a2362986ec76a682f758ab17013
Philip Lutz <philiplutz413@gmail.com>
2019-02-23 12:32:48 -0800

    changing icon on google map and need to host image

--- react-climbers-home/src/components/contact/contact.js
+++ react-climbers-home/src/components/contact/contact.js
@@ -4,7 +4,7 @@ import './contact.css';
 export default class ContactPage extends React.Component {
 	render() {
 		const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
-		// const logoPath = "https://raw.githubusercontent.com/philipLutz/climbers_home/master/hostelPhotos/climbersHomeLogo.svg";
+		const logoPath = "";
 		return (
 			<div id="contact-container">
 				<div id="contact-image">
@@ -39,7 +39,7 @@ export default class ContactPage extends React.Component {
 					</div>
 					<div id="map-image-container">
 						<a href="https://www.google.com/maps/place/488+N+Main+St,+Stanton,+KY+40380/@37.8523327,-83.856699,148m/data=!3m2!1e3!4b1!4m5!3m4!1s0x88439e7ae5b69337:0x52199d2bd0002bc0!8m2!3d37.8523316!4d-83.8561518" target="_blank" rel="noopener noreferrer">
-							<img src={"https://maps.googleapis.com/maps/api/staticmap?center=37.852386,-83.856159&zoom=14&maptype=hybrid&size=300x300&markers=37.852386,-83.856159&markers=color:red%7Clabel:S%7C11211%7C11206%7C11222&markers=size:mid&key=" + GOOGLE_API_KEY} title="Google Map Location" alt="Google Map Location" />
+							<img src={"https://maps.googleapis.com/maps/api/staticmap?center=37.852386,-83.856159&zoom=14&maptype=hybrid&size=300x300&markers=37.852386,-83.856159&markers=color:red%7Clabel:S%7C11211%7C11206%7C11222&markers=size:mid&markers=icon:" + logoPath + "&key=" + GOOGLE_API_KEY} title="Google Map Location" alt="Google Map Location" />
 						</a>
 					</div>
 				</div>
