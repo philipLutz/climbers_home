f1e0c26a8bb0925e251b7d874a9a8b22dbd2a784
Philip Lutz <philiplutz413@gmail.com>
2019-02-27 22:41:54 -0800

    need to finish up rooms page

--- react-climbers-home/src/components/rooms/rooms.js
+++ react-climbers-home/src/components/rooms/rooms.js
@@ -1,4 +1,5 @@
 import React 	from 'react';
+import { Link } from 'react-router-dom';
 import './rooms.css';
 
 export default class RoomsPage extends React.Component {
@@ -10,7 +11,17 @@ export default class RoomsPage extends React.Component {
 					<div className="blur2"></div>
 				</div>
 				<div id="rooms">
-
+					<div id="bedroom-image"></div>
+					<p>
+						Our hostel has several different room options:  single, couples, and family.  Room rates are for one night and do not include tax.  Please check our <Link to="/calendar">calendar</Link> for room availability.
+					</p>
+					<ul>
+						<li>Room 1:  $30 Single</li>
+						<li>Room 2:  $100 Family</li>
+						<li>Room 3 and 4:  $50 Couple</li>
+						<li>Room 5:  $60 Couple</li>
+						<li>Room 6 and 7:  $80 Couple</li>
+					</ul>
 				</div>
 			</div>
 		);
