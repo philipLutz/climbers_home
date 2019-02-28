254dacdf26c892267a22e006e35c6b4d5c3d6953
Philip Lutz <philiplutz413@gmail.com>
2019-02-27 20:14:03 -0800

    working on rooms page

--- react-climbers-home/src/components/rooms/rooms.js
+++ react-climbers-home/src/components/rooms/rooms.js
@@ -5,7 +5,13 @@ export default class RoomsPage extends React.Component {
 	render() {
 		return (
 			<div id="rooms-container">
-				<div id="rooms"></div>
+				<div id="rooms-image">
+					<h2>rooms</h2>
+					<div className="blur2"></div>
+				</div>
+				<div id="rooms">
+
+				</div>
 			</div>
 		);
 	}
