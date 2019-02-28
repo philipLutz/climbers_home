f0adc7dda837a01c37efbc93ec57344208061831
Philip Lutz <philiplutz413@gmail.com>
2019-02-27 20:00:15 -0800

    finished amenities page

--- react-climbers-home/src/components/amenities/amenities.js
+++ react-climbers-home/src/components/amenities/amenities.js
@@ -11,10 +11,12 @@ export default class AmenitiesPage extends React.Component {
 					<div className="blur2"></div>
 				</div>
 				<div id="amenities">
+					<div id="slideshow-container">
+						<Slideshow />
+					</div>
 					<p>
 						In addition to offering a comfy bed in a private room, our hostel has a full kitchen, bathroom, and climbing gym for all guests to use.  There is free Wifi throughout the hostel.  We warmly welcome you to our home and would love for you to enjoy our community and your time spent climbing at the Red River Gorge.  
 					</p>
-					<Slideshow />
 				</div>
 			</div>
 		);
--- react-climbers-home/src/components/amenities/slideshow.js
+++ react-climbers-home/src/components/amenities/slideshow.js
@@ -4,37 +4,43 @@ import Slider                     from "react-slick";
 export default class Slideshow extends Component {
   render() {
     var settings = {
-      dots: true,
-      lazyLoad: true,
+      dots: false,
       fade: true,
       infinite: true,
       slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 500,
+      autoplaySpeed: 3000,
       pauseOnHover: true,
-      adaptiveHeight: true,
+      arrows: true,
+      adaptiveHeight: true
     };
     return (
       <div>
         <Slider {...settings}>
           <div>
-            <img src={} />
+            <img src={"https://github.com/philipLutz/climbers_home/blob/develop/react-climbers-home/src/hostelPhotos/bedroom3.jpg?raw=true"} alt="bedroom3" />
           </div>
           <div>
-            <img src={} />
+            <img src={"https://github.com/philipLutz/climbers_home/blob/develop/react-climbers-home/src/hostelPhotos/climbingGym1.jpg?raw=true"} alt="climbingGym1" />
           </div>
           <div>
-            <img src={} />
+            <img src={"https://github.com/philipLutz/climbers_home/blob/develop/react-climbers-home/src/hostelPhotos/building.jpg?raw=true"} alt="building" />
           </div>
           <div>
-            <img src={} />
+            <img src={"https://github.com/philipLutz/climbers_home/blob/develop/react-climbers-home/src/hostelPhotos/climbingGym2.jpg?raw=true"} alt="climbingGym2" />
           </div>
           <div>
-            <img src={} />
+            <img src={"https://github.com/philipLutz/climbers_home/blob/develop/react-climbers-home/src/hostelPhotos/climbingGym4.jpg?raw=true"} alt="climbingGym4" />
           </div>
           <div>
-            <img src={} />
+            <img src={"https://github.com/philipLutz/climbers_home/blob/develop/react-climbers-home/src/hostelPhotos/climbingGym5.jpg?raw=true"} alt="climbingGym5" />
+          </div>
+          <div>
+            <img src={"https://github.com/philipLutz/climbers_home/blob/develop/react-climbers-home/src/hostelPhotos/bathroom.jpg?raw=true"} alt="bathroom" />
+          </div>
+          <div>
+            <img src={"https://github.com/philipLutz/climbers_home/blob/develop/react-climbers-home/src/hostelPhotos/kitchen.jpg?raw=true"} alt="kitchen" />
           </div>
         </Slider>
       </div>
